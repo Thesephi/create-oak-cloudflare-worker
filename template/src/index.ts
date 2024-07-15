b4fea78db1d328960db8837be336f99115b24dac
dk <2597375+Thesephi@users.noreply.github.com>
2024-07-15 02:01:15 +0200

    Release/1.3.0

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,7 +1,8 @@
 import { Application } from "@oak/oak/application";
-import { useOakServer } from "@dklab/oak-routing-ctrl";
+import { useOakServer, useOas } from "@dklab/oak-routing-ctrl";
 import { CfwController } from "./CfwController";
 
 const app = new Application();
 useOakServer(app, [CfwController]);
+useOas(app);
 export default { fetch: app.fetch };
